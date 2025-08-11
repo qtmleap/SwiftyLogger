930ecd65f0c7536fffc28080f80ad499abce65d5
tkgstrator <nasawake.am@gmail.com>
2025-08-11 23:03:41 +0000

    build(package): update iOS deployment target to v15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyLogger",
-    platforms: [.iOS(.v13), .macOS(.v12)],
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
